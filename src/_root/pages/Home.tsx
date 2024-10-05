cc79434b96bff253b0300240e48e567c4a43c6b9
ShiviiSonii <1111dhruvsoni@gmail.com>
2024-10-05 11:02:53 +0530

    added icon

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -36,6 +36,18 @@ const Home = () => {
       <div className="home-container">
         <div className="home-posts">
           <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+          <div className="flex gap-2 w-full max-w-5xl">
+          {/* Save icon */}
+          <img
+            src="/assets/icons/home.svg"
+            width={36}
+            height={36}
+            alt="edit"
+            className="invert-white"
+          />
+          {/* Title */}
+          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+        </div>
           {isPostLoading && !posts ? (
             <Loader />
           ) : (
